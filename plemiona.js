0f6052702a9bc9989a5d5839b9c9ee79ab5065f6
koQQQ <61546269+koQQQ@users.noreply.github.com>
2020-02-27 21:51:26 +0100

    Update plemiona.js

--- plemiona.js
+++ plemiona.js
@@ -1,4 +1,5 @@
-"use strict";
+(function() {
+    "use strict";
 
 function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
 
@@ -287,4 +288,5 @@ function () {
 
 (function () {
   new CountOffVillages();
+})();
 })();
