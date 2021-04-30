168812dc6a162b78755b54d066e0e843d3701104
koQQQ <61546269+koQQQ@users.noreply.github.com>
2021-04-30 12:43:02 +0200

    Update plemiona.js

--- plemiona.js
+++ plemiona.js
@@ -1,3 +1,5 @@
+javascript: 
+
 "use strict";
 
 function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
